1d3106cde8af5a126ed8abe62bd98a549afc51fa
Pisatel <Pisatel.odinochka@mail.ru>
2024-09-03 20:01:42 +0300

    Added input handling

--- Program.cs
+++ Program.cs
@@ -1,1 +1,17 @@
-﻿
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Ввод массива строк
+        Console.WriteLine("Введите строки через пробел:");
+        string[] inputArray = Console.ReadLine().Split(' ');
+
+        // Вызов метода для фильтрации строк по длине
+        string[] resultArray = FilterStrings(inputArray);
+
+        // Вывод результата
+        Console.WriteLine("Массив строк, длина которых меньше или равна 3 символам:");
+        PrintArray(resultArray);
+    }
